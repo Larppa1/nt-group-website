048c80a7c1dc88e13847582f38b91354fbc0d89c
Larppa1 <lari.juva7@gmail.com>
2022-10-16 23:15:59 +0300

    -

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,11 +1,45 @@
 import './NewsContainer.css'
 
 export default function NewsContainer(props) {
-    return(
-        <article id='newsContainer' className="prose shadow-xl">
-            <h6>{props.date}</h6>
-            <h4>{props.title}</h4>
-            <p>{props.content}</p>
-        </article>
-    )
+    if(window.innerWidth < 768) {
+        return(
+            <article id='newsContainer' className="card w-96 bg-base-100 shadow-xl">
+                <figure>
+                    <img
+                        src="https://placeimg.com/400/225/arch"
+                        alt="Shoes"
+                    />
+                </figure>
+                <div className="card-body">
+                    <article className='prose'>
+                        <h2 className="card-title">{props.title}</h2>
+                        <p>{props.content}</p>
+                    </article>
+                    <div className="card-actions justify-end">
+                        <h6>{props.date}</h6>
+                    </div>
+                </div>
+            </article>
+        )
+    }else {
+        return(
+            <article id='newsContainer' className="card card-compact w-52 bg-base-100 shadow-xl">
+                <figure>
+                    <img
+                        src="https://placeimg.com/400/225/arch"
+                        alt="Shoes"
+                    />
+                </figure>
+                <div className="card-body">
+                    <article className='prose'>
+                        <h2 className="card-title">{props.title}</h2>
+                        <p>{props.content}</p>
+                    </article>
+                    <div className="card-actions justify-end">
+                        <h6>{props.date}</h6>
+                    </div>
+                </div>
+            </article>
+        )
+    }
 }
