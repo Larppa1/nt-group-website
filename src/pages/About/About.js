36e51ebd9a26080f0948070b1d16f73b971a2e74
Larppa1 <lari.juva7@gmail.com>
2022-10-19 22:22:12 +0300

    -

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import Navbar from '../../components//Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import PageTitle from '../../components/PageTitle/PageTitle'
@@ -5,6 +6,11 @@ import './About.css'
 
 export default function About() {
     const pageTitle = "Tietoja"
+    const [faqPosition, setFaqPosition] = useState(0)
+
+    useEffect(() => {
+        setFaqPosition(document.getElementById('faqContainer').getBoundingClientRect().top + 20)
+    }, [])
 
     if(window.innerWidth < 768) {
         return(
@@ -15,9 +21,17 @@ export default function About() {
                 <main id='aboutMain'>
                     <PageTitle title={pageTitle} />
                     <div className="divider"></div>
-                    <article className='prose'>
-                        <h4>HUOM! Usein kysytyt kysymykset löydät <span>FAQ</span>-osiosta.</h4>
-                    </article>
+                    <section>
+                        <article className='prose'>
+                            <h4>Huom! Usein kysytyt kysymykset löydät <span onClick={() => window.scrollTo({
+                                top: faqPosition,
+                                behavior: 'smooth',
+                            })}>FAQ</span>-osiosta.</h4>
+                        </article>
+                    </section>
+                    <section id='faqContainer'>
+
+                    </section>
                 </main>
                 <footer>
                     <Footer />
@@ -30,9 +44,20 @@ export default function About() {
                 <header>
                     <Navbar />
                 </header>
-                <main>
+                <main id='aboutMain'>
                     <PageTitle title={pageTitle} />
                     <div className="divider"></div>
+                    <section>
+                        <article className='prose'>
+                            <h3>Huom! Usein kysytyt kysymykset löydät <span onClick={() => window.scrollTo({
+                                top: faqPosition,
+                                behavior: 'smooth',
+                            })}>FAQ</span>-osiosta.</h3>
+                        </article>
+                    </section>
+                    <section id='faqContainer'>
+
+                    </section>
                 </main>
                 <footer>
                     <Footer />
