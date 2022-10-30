62e8c9e86962aba135a89657bdc6b05071fabcbd
Larppa1 <lari.juva7@gmail.com>
2022-10-30 21:00:32 +0200

    -

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,7 +1,11 @@
 import { useEffect } from 'react'
 import './ContactForm.css'
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 export default function ContactForm() {
+    const form = useRef()
+
     useEffect(() => {
         const emailRadio = document.getElementById('emailRadio')
         const emailInput = document.getElementById('emailInput')
@@ -21,15 +25,27 @@ export default function ContactForm() {
             }
         })
     })
+
+    const sendDetails = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('service_hh87ikt', 'template_6c3dosn', form.current, 'n5MdbvlDxAKYVvX49')
+            .then((result) => {
+                console.log(result.text)
+            })
+            .catch((err) => {
+                console.log(err.text)
+            })
+    }
+
     return (
-        <form id='contactForm'>
+        <form id='contactForm' ref={form}>
             <article className='prose'>
                 <h4>Ota yhteyttä</h4>
             </article>
-            <input type="text" placeholder="Nimi" required="true" className="input input-bordered w-full max-w-xs" />
-            <input type="tel" placeholder="Puhelin" id='phoneInput' className="input input-bordered w-full max-w-xs" />
-            <input type="email" placeholder="Sähköposti" id='emailInput' className="input input-bordered w-full max-w-xs" />
-            <textarea type="text" placeholder="Viesti" required="true" className="input input-bordered w-full max-w-xs" />
+            <input type="text" name="name" placeholder="Nimi" required="true" className="input input-bordered w-full max-w-xs" />
+            <input type="tel" name="phoneNum" placeholder="Puhelin" id='phoneInput' className="input input-bordered w-full max-w-xs" />
+            <input type="email" name="email" placeholder="Sähköposti" id='emailInput' className="input input-bordered w-full max-w-xs" />
+            <textarea type="text" name="message" placeholder="Viesti" required="true" className="input input-bordered w-full max-w-xs" />
             <article className='prose'>
                 <h6>Haluan, että minuun ollaan yhteydessä</h6>
             </article>
--- src/pages/ShareIssue/ShareIssue.js
+++ src/pages/ShareIssue/ShareIssue.js
@@ -2,9 +2,24 @@ import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import PageTitle from '../../components/PageTitle/PageTitle'
 import './ShareIssue.css'
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 export default function ShareIssue() {
     const pageTitle = "Osakeanti"
+    const form = useRef()
+
+    const sendDetails = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('service_hh87ikt', 'template_6c3dosn', form.current, 'n5MdbvlDxAKYVvX49')
+            .then(() => {
+                document.getElementById('emailInput').value = ''
+                document.getElementById('sendConfirmation').style.opacity = 1
+            })
+            .catch((err) => {
+                console.log(err.text)
+            })
+    }
 
     if(window.innerWidth < 768) {
         return(
@@ -19,16 +34,20 @@ export default function ShareIssue() {
                         <article className='prose'>
                             <h4>Kuule ensimmäisten joukossa osakeannistamme liittymällä sähköpostilistalle!</h4>
                         </article>
-                        <form>
+                        <form ref={form} onSubmit={sendDetails}>
                             <input
                                 id='emailInput'
                                 className="input input-bordered w-full max-w-xs"
                                 type="email"
+                                name="user_email"
                                 placeholder="Sähköposti"
                                 required="true"
                             />
                             <button className="btn">Liity</button>
                         </form>
+                    <article className='prose'>
+                        <h4 id='sendConfirmation'>Sähköpostiosoitteesi on vastaanotettu!</h4>
+                    </article>
                     </section>
                 </main>
                 <footer>
@@ -49,16 +68,20 @@ export default function ShareIssue() {
                         <article className='prose'>
                             <h3>Kuule ensimmäisten joukossa osakeannistamme liittymällä sähköpostilistalle!</h3>
                         </article>
-                        <form>
+                        <form ref={form} onSubmit={sendDetails}>
                             <input
                                 id='emailInput'
                                 className="input input-bordered w-full max-w-xs"
                                 type="email"
+                                name="user_email"
                                 placeholder="Sähköposti"
                                 required="true"
                             />
                             <button className="btn">Liity</button>
                         </form>
+                    <article className='prose'>
+                        <h4 id='sendConfirmation'>Sähköpostiosoitteesi on vastaanotettu!</h4>
+                    </article>
                     </section>
                 </main>
                 <footer>
